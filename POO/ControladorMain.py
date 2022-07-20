3bde2918842d55431585501347f9b025d27ddcd4
1000256340c <cristiangsanchez928@gmail.com>
2022-07-20 10:52:53 -0500

    Libreria Tkinter

--- POO/ControladorMain.py
+++ POO/ControladorMain.py
@@ -1,19 +1,69 @@
+
 from tkinter import *
-import tkinter as tk
-import interfaceGrafica
-import sys
-import CRUD
+# iniciar tkinder
+aplicacion = Tk()
 
-# 1 cargamos tareas es decir cargamos nuestro formato json
-tareas = CRUD.Read
+# Tamaño de la ventana
+aplicacion.geometry('1020x630')
 
-# 2 cargar la interface
-ventana = interfaceGrafica.ventanaPrincipal(tareas)
+# Evitar que la pantalla se maximize o se agrande el tamaño de la ventana
+aplicacion.resizable(0, 0)
 
-# 3
-if not (tareas):
-    # la funcion sys.exit(1) --> sirve para salir de un programa
-    sys.exit(1)
+# Titulo de la Ventana
+aplicacion.title("Sistema de Facturacion")
 
-# 4 mostrar ventana
-ventana.mainloop()
+# Color dondo de la ventana
+aplicacion.config(bg='orange')
+
+# Panel superior
+panel_superior = Frame(aplicacion, bd=1, relief=FLAT)
+panel_superior.pack(side=TOP)
+# etiqueta del titulo
+etiqueta_titulo = Label(
+    panel_superior, text='Sistema de Facturacion', fg='azure4', font=('Dosis', 58), bg='burlywood', width=27)
+etiqueta_titulo.grid(row=0, column=0)
+
+# Panel izquierdo
+panel_izquierdo = Frame(aplicacion, bd=1, relief=FLAT)
+panel_izquierdo.pack(side=LEFT)
+
+# Panel Costos
+
+panel_costos = Frame(panel_izquierdo, bd=1, relief=FLAT)
+panel_costos.pack(side=BOTTOM)
+
+# panel comidas
+
+panel_comidas = LabelFrame(panel_izquierdo, text='comidas', font=(
+    'Dosis', 19, 'bold'), bd=1, relief=FLAT, fg='azure4')
+panel_comidas.pack(side=LEFT)
+
+# panel Bebidas
+panel_bebidas = LabelFrame(panel_izquierdo, text='bebidas', font=(
+    'Dosis', 19, 'bold'), bd=1, relief=FLAT, fg='azure4')
+panel_bebidas.pack(side=LEFT)
+
+# panel postres
+panel_postres = LabelFrame(panel_izquierdo, text='bebidas', font=(
+    'Dosis', 19, 'bold'), bd=1, relief=FLAT, fg='azure4')
+panel_postres.pack(side=LEFT)
+
+# panel derecha
+panel_derecha = Frame(aplicacion, bd=1, relief=FLAT)
+panel_derecha.pack(side=RIGHT)
+
+# panel calculadora
+panel_calculadora = Frame(panel_derecha, bd=1, relief=FLAT, bg='burlywood')
+panel_calculadora.pack()
+
+# panel recibo
+panel_recibo = Frame(panel_derecha, bd=1, relief=FLAT, bg='burlywood')
+panel_recibo.pack()
+
+# panel_botones
+panel_botones = Frame(panel_derecha, bd=1, relief=FLAT, bg='burlywood')
+panel_botones.pack()
+
+
+# Para Ejecutar que la ventana se abra y no se cierre
+aplicacion.mainloop()
